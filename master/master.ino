dd8be535abebc91fdc8c638968f39e525de88cdd
Patrick Dantas <patrickdsilva99@gmail.com>
2021-11-09 16:21:09 -0300

    comunicação UDP quase

--- master/master.ino
+++ master/master.ino
@@ -2,6 +2,7 @@
 #include "ESPAsyncWebServer.h"
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+#include "AsyncUDP.h"
 
 #define DHTTYPE DHT11 // Tipo de Sensor DHT 11
 #define DHTPIN 2 // Digital pin connected to the DHT sensor
@@ -10,6 +11,11 @@ DHT dht(DHTPIN, DHTTYPE);
 //WiFi network name and password 
 const char * ssid = "IMEORF";
 const char * pwd = "ricardofranco";
+
+AsyncUDP udp;
+
+String temp_slave1;
+String hum_slave1;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -70,14 +76,26 @@ const char index_html[] PROGMEM = R"rawliteral(
   <h2>ESP32 DHT Server</h2>
   <p>
     <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature</span> 
+    <span class="dht-labels">Temperatura</span> 
     <span id="temperature">%TEMPERATURE%</span>
     <sup class="units">&deg;C</sup>
   </p>
   <p>
     <i class="fas fa-tint" style="color:#00add6;"></i> 
-    <span class="dht-labels">Humidity</span>
+    <span class="dht-labels">Umidade</span>
     <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">&percnt;</sup>
+  </p>
+    <p>
+    <i class="fas fa-thermometer-half" style="color:#9e058c;"></i> 
+    <span class="dht-labels">Temperatura Medidor 1</span> 
+    <span id="temperature_slave1">%TEMPERATURE_SLAVE1%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#d600ab;"></i> 
+    <span class="dht-labels">Umidade Medidor 1</span>
+    <span id="humidity_slave1">%HUMIDITY_SLAVE1%</span>
     <sup class="units">&percnt;</sup>
   </p>
 </body>
@@ -92,7 +110,6 @@ setInterval(function ( ) {
   xhttp.open("GET", "/temperature", true);
   xhttp.send();
 }, 5000 ) ;
-
 setInterval(function ( ) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -101,6 +118,26 @@ setInterval(function ( ) {
     }
   };
   xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 5000 ) ;
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity_slave1").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity_slave1", true);
+  xhttp.send();
+}, 5000 ) ;
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature_slave1").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature_slave1", true);
   xhttp.send();
 }, 5000 ) ;
 </script>
@@ -123,6 +160,7 @@ void setup(){
   dht.begin();
     
   //Connect to the WiFi network
+  WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, pwd);
   Serial.println("");
   
@@ -147,20 +185,43 @@ void setup(){
   server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", readDHTHumidity().c_str());
   });
+  server.on("/temperature_slave1", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", temp_slave1.c_str());
+  });
+  server.on("/humidity_slave1", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", hum_slave1.c_str());
+  });
 
   // Start server
   server.begin();
+
+    if(udp.listen(1234)) {
+        Serial.print("UDP Listening on IP: ");
+        Serial.println(WiFi.localIP());
+        udp.onPacket([](AsyncUDPPacket packet) {
+            Serial.print("UDP Packet Type: ");
+            Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
+            Serial.print(", From: ");
+            Serial.print(packet.remoteIP());
+            Serial.print(":");
+            Serial.print(packet.remotePort());
+            Serial.print(", To: ");
+            Serial.print(packet.localIP());
+            Serial.print(":");
+            Serial.print(packet.localPort());
+            Serial.print(", Length: ");
+            Serial.print(packet.length());
+            Serial.print(", Data: ");
+            Serial.write(packet.data(), packet.length());
+            Serial.println();
+            //reply to the client
+            //packet.printf("Got %u bytes of data", packet.length());
+        });
+    }  
 }
 
 void loop(){
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  float y = 0;
-  //data will be sent to server
-  //buf = char(h);
-  uint8_t buffer[50] = "bbb";
-  //send hello world to server
-  Serial.println(h);
-  Serial.println(t);
-  delay(1000);
+  //delay(1000);
+  //Send broadcast
+  //udp.broadcast("Anyone here?");
 }
--- slave/slave.ino
+++ slave/slave.ino
@@ -1,7 +1,7 @@
 #include "WiFi.h"
-#include "ESPAsyncWebServer.h"
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+#include <AsyncUDP.h>
 
 #define DHTTYPE DHT11 // Tipo de Sensor DHT 11
 #define DHTPIN 2 // Digital pin connected to the DHT sensor
@@ -11,8 +11,7 @@ DHT dht(DHTPIN, DHTTYPE);
 const char * ssid = "IMEORF";
 const char * pwd = "ricardofranco";
 
-// Create AsyncWebServer object on port 80
-AsyncWebServer server(80);
+AsyncUDP udp;
 
 String readDHTTemperature() {
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -44,80 +43,6 @@ String readDHTHumidity() {
   }
 }
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <style>
-    html {
-     font-family: Arial;
-     display: inline-block;
-     margin: 0px auto;
-     text-align: center;
-    }
-    h2 { font-size: 3.0rem; }
-    p { font-size: 3.0rem; }
-    .units { font-size: 1.2rem; }
-    .dht-labels{
-      font-size: 1.5rem;
-      vertical-align:middle;
-      padding-bottom: 15px;
-    }
-  </style>
-</head>
-<body>
-  <h2>ESP32 DHT Server</h2>
-  <p>
-    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature</span> 
-    <span id="temperature">%TEMPERATURE%</span>
-    <sup class="units">&deg;C</sup>
-  </p>
-  <p>
-    <i class="fas fa-tint" style="color:#00add6;"></i> 
-    <span class="dht-labels">Humidity</span>
-    <span id="humidity">%HUMIDITY%</span>
-    <sup class="units">&percnt;</sup>
-  </p>
-</body>
-<script>
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("temperature").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/temperature", true);
-  xhttp.send();
-}, 5000 ) ;
-
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("humidity").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/humidity", true);
-  xhttp.send();
-}, 5000 ) ;
-</script>
-</html>)rawliteral";
-
-// Replaces placeholder with DHT values
-String processor(const String& var){
-  //Serial.println(var);
-  if(var == "TEMPERATURE"){
-    return readDHTTemperature();
-  }
-  else if(var == "HUMIDITY"){
-    return readDHTHumidity();
-  }
-  return String();
-}
-
 void setup(){
   Serial.begin(115200);
   dht.begin();
@@ -137,30 +62,40 @@ void setup(){
   Serial.print("endereço IP: ");
   Serial.println(WiFi.localIP());
   
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readDHTTemperature().c_str());
-  });
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readDHTHumidity().c_str());
-  });
+  if(udp.connect(IPAddress(192,168,0,153), 1234)) {
+        Serial.println("UDP connected");
+        udp.onPacket([](AsyncUDPPacket packet) {
+            Serial.print("UDP Packet Type: ");
+            Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
+            Serial.print(", From: ");
+            Serial.print(packet.remoteIP());
+            Serial.print(":");
+            Serial.print(packet.remotePort());
+            Serial.print(", To: ");
+            Serial.print(packet.localIP());
+            Serial.print(":");
+            Serial.print(packet.localPort());
+            Serial.print(", Length: ");
+            Serial.print(packet.length());
+            Serial.print(", Data: ");
+            Serial.write(packet.data(), packet.length());
+            Serial.println();
+            //reply to the client
+            //packet.printf("Got %u bytes of data", packet.length());
+        });
+        //Send unicast
+        //udp.print("Hello Server!");
+    }
+    else{
+      Serial.println("UDP failed");
+    }
 
-  // Start server
-  server.begin();
 }
 
 void loop(){
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  float y = 0;
-  //data will be sent to server
-  //buf = char(h);
-  uint8_t buffer[50] = "bbb";
-  //send hello world to server
-  Serial.println(h);
-  Serial.println(t);
-  delay(1000);
+    delay(5000);
+    //Send broadcast on port 1234
+    udp.broadcastTo("Anyone here?", 1234);
+    Serial.println("pacote udp enviado");
+    
 }
